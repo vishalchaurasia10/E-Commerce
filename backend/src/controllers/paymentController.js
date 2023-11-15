24670477bdca19794a8b6dcf8c95a3c756758c7d
vishalchaurasia10 <1ms21cs138@msrit.edu>
2023-11-15 17:21:35 +0530

    linked shiprocket to payment

--- backend/src/controllers/paymentController.js
+++ backend/src/controllers/paymentController.js
@@ -2,6 +2,7 @@ const razorpayInstance = require('../middlewares/razorpay');
 const Products = require('../models/Products'); // Import your Product model
 const Order = require('../models/Order'); // Import your Order model
 const crypto = require('crypto');
+const axios = require('axios');
 
 exports.checkout = async (req, res) => {
     try {
@@ -46,7 +47,6 @@ exports.checkout = async (req, res) => {
 
 exports.verifyTransaction = async (req, res) => {
     try {
-        console.log(req.body);
         const { razorpay_payment_id, razorpay_order_id, razorpay_signature } = req.body;
         const generatedSignature = crypto.createHmac('sha256', process.env.RAZORPAY_KEY_SECRET)
             .update(razorpay_order_id + "|" + razorpay_payment_id)
@@ -77,7 +77,13 @@ exports.verifyTransaction = async (req, res) => {
                 phoneNumber: req.body.phoneNumber
             });
 
-            await order.save();
+            const createdOrder = await order.save();
+
+            const accessToken = await getShiprocketAccessToken();
+            const shiprocketOrder = await mapOrderToShiprocketFormat(createdOrder);
+
+            await createShiprocketOrder(accessToken, shiprocketOrder);
+
             res.status(200).json({ status: "Success", message: "Order placed successfully" });
         } else {
             res.status(400).json({ status: "Failure", message: "Order Verification Failed" });
@@ -85,5 +91,113 @@ exports.verifyTransaction = async (req, res) => {
     } catch (error) {
         console.log(error);
         res.status(500).send(error);
+    }
+}
+
+async function createShiprocketOrder(shiprocketAccessToken, shiprocketOrder) {
+    try {
+        const response = await axios.post('https://apiv2.shiprocket.in/v1/external/orders/create/adhoc', shiprocketOrder, {
+            headers: {
+                Authorization: `Bearer ${shiprocketAccessToken}`,
+            },
+        });
+        console.log("Shiprocket Order Created:", response.data);
+    } catch (error) {
+        console.error("Error creating Shiprocket order:", error.response ? error.response.data : error.message);
+    }
+}
+
+async function getShiprocketAccessToken() {
+    try {
+        const response = await axios.post('https://apiv2.shiprocket.in/v1/external/auth/login', {
+            email: process.env.SHIPROCKET_EMAIL,
+            password: process.env.SHIPROCKET_PASSWORD
+        });
+        console.log("Shiprocket Access Token:", response.data.token);
+        return response.data.token;
+    } catch (error) {
+        console.error("Error getting Shiprocket access token:", error.response ? error.response.data : error.message);
+    }
+}
+
+async function mapOrderToShiprocketFormat(createdOrder) {
+    const productIds = createdOrder.products.map(item => item._id);
+    // Fetch product details from the database for the product IDs in the cart
+    const products = await Products.find({ _id: { $in: productIds } });
+
+    const priceMap = new Map();
+    const titleMap = new Map();
+
+    products.forEach(product => {
+        priceMap.set(product._id.toString(), product.price);
+    });
+
+    products.forEach(product => {
+        titleMap.set(product._id.toString(), product.title);
+    });
+
+    return {
+        order_id: createdOrder.orderId,
+        order_date: createdOrder.createdAt,
+        phone_verified: 1,
+        id: createdOrder._id,
+        pickup_location: "Forever TrendIn",
+        address: "H no 336, beside Mohini Apartment, Goushala Road",
+        address_2: "Near Jain Mandir, Jugsalai",
+        city: "East Singhbhum",
+        email: "forevertrendin1429@gmail.com",
+        phone: "9341643917",
+        seller_name: "Forever TrendIn",
+        state: "Jharkhand",
+        country: "India",
+        status: 2,
+        pin_code: "831006",
+        lat: "",
+        long: "",
+        warehouse_code: null,
+        channel_id: "",
+        comment: "Reseller: M/s Goku",
+        billing_customer_name: createdOrder.firstName,
+        billing_last_name: createdOrder.lastName,
+        billing_address: createdOrder.address,
+        billing_address_2: "",
+        billing_city: createdOrder.city,
+        billing_pincode: createdOrder.pinCode,
+        billing_state: createdOrder.state,
+        billing_country: "India",
+        billing_email: createdOrder.email,
+        billing_phone: createdOrder.phoneNumber,
+        shipping_is_billing: true,
+        shipping_customer_name: "",
+        shipping_last_name: "",
+        shipping_address: "",
+        shipping_address_2: "",
+        shipping_city: "",
+        shipping_pincode: "",
+        shipping_country: "",
+        shipping_state: "",
+        shipping_email: "",
+        shipping_phone: "",
+        order_items: createdOrder.products.map(item => ({
+            name: titleMap.get(item._id.toString()),
+            sku: item._id,
+            units: item.quantity,
+            discount: 0,
+            tax: 0,
+            hsn: 0,
+            selling_price: priceMap.get(item._id.toString()),
+            color: item.color,
+            size: item.size,
+        })),
+        payment_method: "Prepaid",
+        shipping_charges: 0,
+        giftwrap_charges: 0,
+        transaction_charges: 0,
+        total_discount: 0,
+        sub_total: createdOrder.paidAmount / 100,
+        length: 10,
+        breadth: 15,
+        height: 20,
+        weight: 2.5
     }
 }

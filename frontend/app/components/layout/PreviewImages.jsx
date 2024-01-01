6c260812d0c23ad01765eff990ce87bfffc2ec30
vishalchaurasia10 <1ms21cs138@msrit.edu>
2024-01-01 17:30:38 +0530

    bug fixes #3

--- frontend/app/components/layout/PreviewImages.jsx
+++ frontend/app/components/layout/PreviewImages.jsx
@@ -11,12 +11,12 @@ const PreviewImages = ({ imageId }) => {
     return (
         <div className="preview-images flex flex-col space-y-3">
             <div className="large-image">
-                <img className='w-[100%]' src={`${selectedImage}`} alt="Large Preview" />
+                <img className='w-[100%] h-[32rem] object-cover' src={`${selectedImage}`} alt="Large Preview" />
             </div>
             <div className="flex w-3/4 space-x-2 overflow-x-auto scrollbar-hidden">
                 {imageId.map((imageUrl, index) => (
                     <img
-                        className='w-24 cursor-pointer'
+                        className='w-24 h-28 object-cover cursor-pointer'
                         key={index}
                         src={`${imageUrl}`}
                         alt={`Preview ${index + 1}`}

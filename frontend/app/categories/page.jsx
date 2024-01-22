4d44e12bb7994295b03499658815e988c6df016d
vishalchaurasia10 <1ms21cs138@msrit.edu>
2024-01-22 18:28:03 +0530

    fixes: bug fixes

--- frontend/app/categories/page.jsx
+++ frontend/app/categories/page.jsx
@@ -3,8 +3,10 @@ import React, { useEffect, useState } from 'react'
 import { getAllCategories, getCategoryByType } from '@/app/utils/apiFunctions/categoryFunctions'
 import { toast, Toaster } from 'react-hot-toast'
 import Image from 'next/image'
+import Link from 'next/link'
 
 const page = () => {
+    const [loading, setLoading] = useState(false)
     const [totalPages, setTotalPages] = useState(0)
     const [categoryType, setCategoryType] = useState('all')
     const [categories, setCategories] = useState([]);
@@ -16,21 +18,26 @@ const page = () => {
     };
 
     useEffect(() => {
+        setLoading(true)
         if (categoryType === 'all') {
             getAllCategories(page).then(res => {
                 setCategories(res.categories);
                 setTotalPages(Math.ceil(res.count / 12))
+                setLoading(false)
             }).catch(err => {
                 toast.error(err.message);
                 console.log(err);
+                setLoading(false)
             });
         } else {
             getCategoryByType(categoryType, page).then(res => {
                 setCategories(res.categories);
                 setTotalPages(Math.ceil(res.count / 12))
+                setLoading(false)
             }).catch(err => {
                 toast.error(err.message);
                 console.log(err);
+                setLoading(false)
             });
         }
     }, [categoryType, page]);
@@ -42,6 +49,7 @@ const page = () => {
     return (
         <>
             <Toaster />
+            <h1 className='font-bebas_neue text-4xl text-center lg:text-6xl pt-5 mb-5'>Categories</h1>
             <div className="mainContainer min-h-[35rem] flex flex-col lg:flex-row lg:space-x-4 bg-white justify-center px-10">
                 <div className="filter lg:w-[18%] border-2 border-gray-400 my-2 p-4 h-fit lg:sticky lg:top-20">
                     {
@@ -55,42 +63,48 @@ const page = () => {
                         })
                     }
                 </div>
-                <div className="showCategories lg:w-[82%] flex flex-col flex-wrap bg-white">
-                    {/* <h2 className='font-roboto block pl-20 text-lg'>Category count: <span className='font-bold'>{count}</span></h2> */}
-                    <div className="categories flex px-10 flex-wrap">
-                        {categories && categories.length > 0 && categories.map((category, index) => (
-                            <React.Fragment key={category._id}>
-                                <div className="carousel-item m-2 w-60">
-                                    <div
-                                        id={`slide${index + 1}`}
-                                        className={`carousel-item w-60`}
-                                    >
-                                        <div className='relative w-full flex flex-col border border-gray-400 border-opacity-25 shadow-lg shadow-gray-400'>
-                                            <Image height={500} width={500} src={`${category.coverImageId}`} className="w-full h-[18rem] object-cover" alt={`Slide ${index + 1}`} />
-                                            <h3 className={`text-black pl-4 pt-3`}><span className='font-bold'>Title: </span>{category.title}</h3>
-                                            <h3 className={`text-black pl-4 pb-3`}><span className='font-bold'>Type: </span> {category.type}</h3>
+                {
+                    loading ?
+                        <div className="w-full flex justify-center my-10 items-start">
+                            <span className="loading loading-spinner loading-lg"></span>
+                        </div>
+                        :
+                        <div className="showCategories lg:w-[82%] flex flex-col flex-wrap bg-white">
+                            <div className="categories flex px-10 flex-wrap">
+                                {categories && categories.length > 0 && categories.map((category, index) => (
+                                    <Link key={category._id} href={`/collection?categoryId=${category._id}`}>
+                                        <div className="carousel-item m-2 w-60">
+                                            <div
+                                                id={`slide${index + 1}`}
+                                                className={`carousel-item w-60`}
+                                            >
+                                                <div className='relative w-full flex flex-col border border-gray-400 border-opacity-25 shadow-lg shadow-gray-400'>
+                                                    <Image height={500} width={500} src={`${category.coverImageId}`} className="w-full h-[18rem] object-cover" alt={`Slide ${index + 1}`} />
+                                                    <h3 className={`text-black pl-4 pt-3`}><span className='font-bold'>Title: </span>{category.title}</h3>
+                                                    <h3 className={`text-black pl-4 pb-3`}><span className='font-bold'>Type: </span> {category.type}</h3>
+                                                </div>
+                                            </div>
                                         </div>
-                                    </div>
-                                </div>
-                            </React.Fragment>
-                        ))}
-                    </div>
-                    <div className="join w-full flex justify-center my-10">
-                        <button
-                            onClick={() => handlePageChange(page - 1)}
-                            disabled={page === 1}
-                            className="join-item btn">
-                            «
-                        </button>
-                        <button className="join-item btn">Page {page}</button>
-                        <button
-                            onClick={() => handlePageChange(page + 1)}
-                            disabled={page >= totalPages}
-                            className="join-item btn">
-                            »
-                        </button>
-                    </div>
-                </div>
+                                    </Link>
+                                ))}
+                            </div>
+                            <div className="join w-full flex justify-center my-10">
+                                <button
+                                    onClick={() => handlePageChange(page - 1)}
+                                    disabled={page === 1}
+                                    className="join-item btn">
+                                    «
+                                </button>
+                                <button className="join-item btn">Page {page}</button>
+                                <button
+                                    onClick={() => handlePageChange(page + 1)}
+                                    disabled={page >= totalPages}
+                                    className="join-item btn">
+                                    »
+                                </button>
+                            </div>
+                        </div>
+                }
             </div>
         </>
     )

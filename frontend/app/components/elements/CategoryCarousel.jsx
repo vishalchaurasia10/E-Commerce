5c76c6051befb37de3900911b557347f69f3406f
vishalchaurasia10 <1ms21cs138@msrit.edu>
2024-03-23 23:59:34 +0530

    fixed the category carousel on all the pages

--- frontend/app/components/elements/CategoryCarousel.jsx
+++ frontend/app/components/elements/CategoryCarousel.jsx
@@ -37,11 +37,12 @@ const CategoryCarousel = ({ type }) => {
                                     <div
                                         key={category._id}
                                         id={`slide${index + 1}`}
-                                        className={`carousel-item w-60 h-[22rem]`}
+                                        className={`carousel-item w-60 h-[18rem]`}
                                     >
-                                        <div className='flex flex-col w-full items-center justify-center border border-gray-400 border-opacity-25 shadow-lg shadow-gray-400 overflow-hidden'>
+                                        <div className='flex w-full justify-center overflow-hidden relative'>
                                             <Image width={500} height={500} src={`${category.coverImageId}`} className="w-full h-[18rem] object-cover" alt={`Slide ${index + 1}`} loading='lazy' />
-                                            <h3 className={`font-bebas_neue py-5 text-black text-2xl text-center`}>{category.title}</h3>
+                                            <div className="absolute inset-0 top-1/2 bg-gradient-to-b from-transparent via-opacity-30 to-black" />
+                                            <h3 className={`font-bebas_neue py-5 bottom-0 text-white text-2xl text-center absolute`}>{category.title}</h3>
                                         </div>
                                     </div>
                                 </Link>

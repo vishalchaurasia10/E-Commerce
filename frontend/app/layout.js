d246912875b912581996565b7206b79637e4fb46
vishalchaurasia10 <1ms21cs138@msrit.edu>
2024-09-02 18:02:34 +0530

    fixed fb meta code

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -29,29 +29,22 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${roboto.className}`}>
-        <Script
-          id="fb-pixel"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '1234202434674429');
-              fbq('track', 'PageView');
-            `,
-          }}
-        />
+        <Script id="meta-pixel-script" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1234202434674429');
+            fbq('track', 'PageView');
+          `}
+        </Script>
         <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: 'none' }}
+          <img height="1" width="1" style={{ display: 'none' }}
             src="https://www.facebook.com/tr?id=1234202434674429&ev=PageView&noscript=1"
           />
         </noscript>
